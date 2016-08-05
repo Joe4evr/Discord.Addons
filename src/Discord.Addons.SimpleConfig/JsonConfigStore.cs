8a3e9871326e8c29f33ae5856dcb8786c36cb115
Joe4evr <jii.geugten@gmail.com>
2016-08-05 19:32:26 +0200

    Genericize the included JsonConfigStore Bump version number

--- src/Discord.Addons.SimpleConfig/JsonConfigStore.cs
+++ src/Discord.Addons.SimpleConfig/JsonConfigStore.cs
@@ -6,14 +6,15 @@ namespace Discord.Addons.SimpleConfig
 {
     /// <summary>
     /// Basic implementation of <see cref="IConfigStore{TConfig}"/> that stores and loads
-    /// an <see cref="IConfig"/> as JSON on disk using JSON.NET.
+    /// a configuration object as JSON on disk using JSON.NET.
     /// </summary>
-    public sealed class JsonConfigStore : IConfigStore<IConfig>
+    /// <typeparam name="TConfig"></typeparam>
+    public sealed class JsonConfigStore<TConfig> : IConfigStore<TConfig> where TConfig : IConfig
     {
         private readonly string _jsonPath;
 
         /// <summary>
-        /// Initializes a new instance of <see cref="JsonConfigStore"/>.
+        /// Initializes a new instance of <see cref="JsonConfigStore{TConfig}"/>.
         /// Will create a new file if it does not exist.
         /// </summary>
         /// <param name="path">Path to the JSON file.</param>
@@ -25,12 +26,20 @@ namespace Discord.Addons.SimpleConfig
             _jsonPath = path;
         }
 
-        public IConfig Load()
+        /// <summary>
+        /// Load the configuration from disk.
+        /// </summary>
+        /// <returns>The configuration object.</returns>
+        public TConfig Load()
         {
-            return JsonConvert.DeserializeObject<IConfig>(File.ReadAllText(_jsonPath));
+            return JsonConvert.DeserializeObject<TConfig>(File.ReadAllText(_jsonPath));
         }
 
-        public void Save(IConfig config)
+        /// <summary>
+        /// Saves the configuration object to disk.
+        /// </summary>
+        /// <param name="config">The configuration object.</param>
+        public void Save(TConfig config)
         {
             File.WriteAllText(_jsonPath, JsonConvert.SerializeObject(config));
         }
