ed801ebfaaabafea6b024160bd74124f4cc09891
Joe4evr <jii.geugten@gmail.com>
2017-02-12 14:50:29 +0100

    [Trivia] Misc

---
+++ src/Discord.Addons.Trivia/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Discord.Addons.TriviaGames
+{
+    internal static class Extensions
+    {
+        public static TValue GetValueOrDefault<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue = default(TValue))
+        {
+            return dictionary.TryGetValue(key, out var ret) ? ret : defaultValue;
+        }
+    }
+}
--- src/Discord.Addons.Trivia/TriviaModuleBase.cs
+++ src/Discord.Addons.Trivia/TriviaModuleBase.cs
@@ -7,13 +7,13 @@ namespace Discord.Addons.TriviaGames
     public abstract class TriviaModuleBase : ModuleBase<ICommandContext>
     {
         /// <summary> Indicates if a game is already being played. </summary>
-        protected bool GameInProgress;
+        protected bool GameInProgress { get; private set; }
 
         /// <summary> The instance of the game being played, if so. </summary>
-        protected TriviaGame Game;
+        protected TriviaGame Game { get; private set; }
 
         /// <summary> The instance of the <see cref="TriviaService"/>. </summary>
-        protected readonly TriviaService Service;
+        protected TriviaService Service { get; }
 
         /// <summary> </summary>
         /// <param name="service"></param>
@@ -22,10 +22,11 @@ namespace Discord.Addons.TriviaGames
             Service = service;
         }
 
-        //protected override void BeforeExecute()
-        //{
-        //    GameInProgress = Service.TriviaGames.TryGetValue(Context.Channel.Id, out Game);
-        //}
+        protected override void BeforeExecute()
+        {
+            Game = Service.TriviaGames.GetValueOrDefault(Context.Channel.Id);
+            GameInProgress = Game != null;
+        }
 
         /// <summary> Creates a new Trivia game. </summary>
         /// <param name="turns">Amount of turns the game should last.</param>
