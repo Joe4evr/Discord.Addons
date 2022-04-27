a3203cde7a4038584709942e7c36b11d42ef7526
Joe4evr <jii.geugten@gmail.com>
2022-04-27 21:44:43 +0200

    Slight rewording of comments

--- src/Discord.Addons.SimpleAudio/AllocExtensions.cs
+++ src/Discord.Addons.SimpleAudio/AllocExtensions.cs
@@ -13,14 +13,14 @@ internal static class AllocExtensions
     {
         // Calculate a slightly larger buffer size to compensate
         // for platforms that require address-aligned access (like ARM).
-        var alignment = AlignmentOf<TAlignFor>();
-        var actualSize = requestedSize + alignment - 1;
+        var alignment = AlignmentOf<TAlignFor>() - 1;
+        var actualSize = requestedSize + alignment;
 
-        // Ensuring the buffer is never moved by GC should save some cycles.
+        // Ensure the buffer is never moved by GC so the alignment stays consistent.
         // Also save cycles from skipping zero-init since we don't read before writing.
         byte[] buffer = GC.AllocateUninitializedArray<byte>(actualSize, pinned: true);
         var address = (nint)Unsafe.AsPointer(ref MemoryMarshal.GetArrayDataReference(buffer));
-        var offset = (int)(((address + alignment - 1) & (~(alignment - 1))) - address);
+        var offset = (int)(((address + alignment) & (~alignment)) - address);
 
         // If 'offset' ended up non-zero,
         // clearing out the leading bytes
@@ -39,6 +39,8 @@ internal static class AllocExtensions
     private readonly struct AlignmentHelper<T>
         where T : unmanaged
     {
+        // Are these fields affected if an end-dev enables trimming?
+        // If so, how to prevent?
         internal readonly byte _padding;
         internal readonly T _value;
     }
